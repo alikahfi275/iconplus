6b61c75ca5551e0f6d42b2a64737fdc726fac60b
ali.lubis <ali.lubis@quadrant-si.id>
2025-04-19 19:58:00 +0700

    add splashscreen

--- App.tsx
+++ App.tsx
@@ -3,10 +3,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from '../iconplus/src/screens/HomeScreen';
 import Login from '../iconplus/src/screens/LoginScreens';
+import {useEffect} from 'react';
+import {Platform} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      if (Platform.OS === 'android') SplashScreen.hide();
+    }, 1500);
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator
--- android/app/src/main/java/com/iconplus/MainActivity.java
+++ android/app/src/main/java/com/iconplus/MainActivity.java
@@ -4,11 +4,14 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,9 +19,17 @@ public class MainActivity extends ReactActivity {
     return "iconplus";
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);
+    super.onCreate(savedInstanceState);
+  }
+
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
    * (aka React 18) with two boolean flags.
    */
   @Override
