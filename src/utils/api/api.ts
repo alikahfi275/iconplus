09cd7a0b9f5d557b1efe555342bd4b047f1f9607
ali.lubis <ali.lubis@quadrant-si.id>
2025-05-20 21:46:02 +0700

    change base url

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,5 +1,1 @@
-// Jaringan Kantor
-export const BASE_URL = 'http://10.224.22.28:8888/inven-api/';
-// Jaringan Rumah
-// export const BASE_URL = 'http://192.168.1.5:8888/inven-api/';
-// export const BASE_URL = 'http://192.168.1.8:8888/inven-api/';
+export const BASE_URL = 'http://192.168.43.7/inven-api/';
